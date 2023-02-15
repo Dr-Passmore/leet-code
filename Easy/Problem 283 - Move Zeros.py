ece1bbe002967fe91275f33f5ba7622e9d3473d1
Phillip Passmore <phillipspassmore@gmail.com>
2023-02-15 20:49:28 +0000

    Problem 283 completed
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

---
+++ Easy/Problem 283 - Move Zeros.py
@@ -0,0 +1,25 @@
+import unittest
+
+class Solution:
+    def moveZeroes(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.insert(length-1, i)
+                
+        return nums
+                
+class Test(unittest.TestCase):
+    def test_case1(self):
+        test = Solution.moveZeroes(self,[0,1,0,3,12])
+        self.assertEqual(test, [1,3,12,0,0], "Error")
+    
+    def test_case2(self):
+        test = Solution.moveZeroes(self, [0])
+        self.assertEqual(test, [0], "Error")
+        
+unittest.main()
